e62c98cde1c81aebefca1bc39c4c0274d919b196
leroidangleterre <leroidangleterre@gmail.com>
2020-06-21 14:21:33 +0200

    Multiple clients handling

--- BlenderParallelRendering.java
+++ BlenderParallelRendering.java
@@ -17,6 +17,9 @@ import java.net.Socket;
  */
 public class BlenderParallelRendering {
 
+    public static int IMAGE_INDEX = 0;
+    public static final int MAX_IMAGE_INDEX = 20;
+
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
@@ -28,15 +31,27 @@ public class BlenderParallelRendering {
         InetAddress localhost = InetAddress.getLocalHost();
         System.out.println("address: " + localhost.getHostAddress());
 
-        Socket clientSocket = serverSocket.accept();
-        new Thread(new ConnectionHandler(clientSocket)).start();
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            new Thread(new ConnectionHandler(clientSocket)).start();
+        }
 
+    }
+
+    /**
+     * Increment the index and return the new value.
+     * Synchronization makes sure two threads will receive different values.
+     *
+     * @return the image index that the thread must render.
+     */
+    public static synchronized int getNextImageIndex() {
+        IMAGE_INDEX++;
+        return IMAGE_INDEX;
     }
 
     private static class ConnectionHandler implements Runnable {
 
         String fromClient;
-        int renderIndex = 1;
         Socket clientSocket;
 
         public ConnectionHandler(Socket s) {
@@ -50,18 +65,24 @@ public class BlenderParallelRendering {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String outputLine;
-                while (renderIndex < 40) {
-                    outputLine = "server_asks_for " + renderIndex;
-                    System.out.println("Sending request for " + outputLine);
+
+                boolean loop = true;
+
+                while (loop) {
+                    int index = getNextImageIndex();
+                    outputLine = "server_asks_for " + index;
                     out.println(outputLine);
                     do {
                         fromClient = in.readLine();
                     } while (fromClient.isEmpty());
 
                     System.out.println("    client replied: <" + fromClient + ">");
-                    renderIndex++;
 
+                    if (IMAGE_INDEX > MAX_IMAGE_INDEX) {
+                        loop = false;
+                    }
                 }
+                out.println("END");
             } catch (IOException e) {
                 System.out.println("Error in handling connection");
             }
--- script.py
+++ script.py
@@ -17,24 +17,41 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((address, port))
 
 
-while(True):
+loop = True
+while(loop):
     # print("     start loop ---------------------")
     # Read from the server
     # print("     waiting for server to talk")
     data = s.recv(1000).decode()
 #    print("received from server:")
 #    print("     "+data)
+    if (data.find("END") >= 0):
+        # end detected.
+        loop = False
+        print("Detecting end")
+    else:
+    #    data.replace('\n', ' ');
+    #    data.replace('\r', ' ');
+        
+        imageIndex = data.split(" ")[1]
+        imageIndex.replace("\n", "")
+        
+        imageIndex = int(imageIndex)
+        
+        print("     received from server <"+str(imageIndex)+">")
+    #    time.sleep(2)
+        
+        # Go to the requested frame
+        scene.frame_set(int(imageIndex))
+        scene.render.filepath = filepath + str(imageIndex)
+        
+        # Render the frame
+        bpy.ops.render.render(write_still = True)
+        
+        # Send reply to server
+        # print("     Sending to server...")
+        stringToSend = "            client ONE: rendered "+str(imageIndex)+"\n"
+        s.send(stringToSend.encode())
+        # print("     Sending to server DONE")
 
-    data.replace('\n', ' ');
-    data.replace('\r', ' ');
-    
-    imageIndex = data.split(" ")[1]
-    
-    print("     received from server <"+imageIndex+">")
-    time.sleep(1)
-    
-    # Send reply to server
-    # print("     Sending to server...")
-    stringToSend = "NODE ONE rendered "+imageIndex+"\n"
-    s.send(stringToSend.encode())
-    # print("     Sending to server DONE")
+print("program terminated.")
