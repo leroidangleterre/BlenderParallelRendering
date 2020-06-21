c17f2c694ed0146ac598d843c942b4a5b78154da
leroidangleterre <leroidangleterre@gmail.com>
2020-06-21 15:20:41 +0200

    added client name in server display

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import socket
 import time
 
 scene = bpy.context.scene
-initFilepath = scene.render.filepath # get the current output path
+initFilepath = scene.render.filepath # remember the current output path
 
 port = 65432
 address = '192.168.1.88'
@@ -13,33 +13,27 @@ address = '192.168.1.88'
 print("START ----------------------------------------------")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.connect((address, port))
+hostname = socket.gethostname()
 
 try:
     loop = True
     while(loop):
-        # print("     start loop ---------------------")
+
         # Read from the server
-        # print("     waiting for server to talk")
         data = s.recv(1000).decode()
-    #    print("received from server:")
-    #    print("     "+data)
         if (data.find("END") >= 0):
             # end detected.
             loop = False
             print("Detecting end")
+
         else:
-        #    data.replace('\n', ' ');
-        #    data.replace('\r', ' ');
-            
             imageIndex = data.split(" ")[1]
             imageIndex.replace("\n", "")
             
             imageIndex = int(imageIndex)
             
             print("     received from server <"+str(imageIndex)+">")
-        #    time.sleep(2)
             
             # Go to the requested frame
             scene.frame_set(int(imageIndex))
@@ -49,12 +43,13 @@ try:
             bpy.ops.render.render(write_still = True)
             
             # Send reply to server
-            # print("     Sending to server...")
-            stringToSend = "            client ONE: rendered "+str(imageIndex)+"\n"
+            stringToSend = hostname + " rendered "+str(imageIndex)+"\n"
             s.send(stringToSend.encode())
-            # print("     Sending to server DONE")
+
 except ConnectionResetError as e:
     print("Connection reset by server.")
 
+# Reset the output filepath
 scene.render.filepath = initFilepath
+
 print("program terminated.")
