041c3bc1c812a9f93af0119ff11d060d1f26bb58
leroidangleterre <leroidangleterre@gmail.com>
2020-06-21 14:37:12 +0200

    handled server disconnection

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import socket
 import time
 
 scene = bpy.context.scene
-filepath = scene.render.filepath # get the current output path
+initFilepath = scene.render.filepath # get the current output path
 
 port = 65432
 address = '192.168.1.88'
@@ -16,42 +16,45 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((address, port))
 
+try:
+    loop = True
+    while(loop):
+        # print("     start loop ---------------------")
+        # Read from the server
+        # print("     waiting for server to talk")
+        data = s.recv(1000).decode()
+    #    print("received from server:")
+    #    print("     "+data)
+        if (data.find("END") >= 0):
+            # end detected.
+            loop = False
+            print("Detecting end")
+        else:
+        #    data.replace('\n', ' ');
+        #    data.replace('\r', ' ');
+            
+            imageIndex = data.split(" ")[1]
+            imageIndex.replace("\n", "")
+            
+            imageIndex = int(imageIndex)
+            
+            print("     received from server <"+str(imageIndex)+">")
+        #    time.sleep(2)
+            
+            # Go to the requested frame
+            scene.frame_set(int(imageIndex))
+            scene.render.filepath = initFilepath + str(imageIndex)
+            
+            # Render the frame
+            bpy.ops.render.render(write_still = True)
+            
+            # Send reply to server
+            # print("     Sending to server...")
+            stringToSend = "            client ONE: rendered "+str(imageIndex)+"\n"
+            s.send(stringToSend.encode())
+            # print("     Sending to server DONE")
+except ConnectionResetError as e:
+    print("Connection reset by server.")
 
-loop = True
-while(loop):
-    # print("     start loop ---------------------")
-    # Read from the server
-    # print("     waiting for server to talk")
-    data = s.recv(1000).decode()
-#    print("received from server:")
-#    print("     "+data)
-    if (data.find("END") >= 0):
-        # end detected.
-        loop = False
-        print("Detecting end")
-    else:
-    #    data.replace('\n', ' ');
-    #    data.replace('\r', ' ');
-        
-        imageIndex = data.split(" ")[1]
-        imageIndex.replace("\n", "")
-        
-        imageIndex = int(imageIndex)
-        
-        print("     received from server <"+str(imageIndex)+">")
-    #    time.sleep(2)
-        
-        # Go to the requested frame
-        scene.frame_set(int(imageIndex))
-        scene.render.filepath = filepath + str(imageIndex)
-        
-        # Render the frame
-        bpy.ops.render.render(write_still = True)
-        
-        # Send reply to server
-        # print("     Sending to server...")
-        stringToSend = "            client ONE: rendered "+str(imageIndex)+"\n"
-        s.send(stringToSend.encode())
-        # print("     Sending to server DONE")
-
+scene.render.filepath = initFilepath
 print("program terminated.")
